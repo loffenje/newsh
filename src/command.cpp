7ee0c0c7071fe8291db4cdea0e02d728d2893d9d
loffenje <coda.flow@gmail.com>
2020-01-21 17:00:00 +0200

    command

--- src/command.cpp
+++ src/command.cpp
@@ -1,11 +1,12 @@
 #include <unistd.h>
 #include <fstream>
+#include <iostream>
 
 #include "command.h"
 
 namespace newsh {
     
-    std::pair<std::string, func> findCommand(const char *name)
+    std::pair<std::string, func> findCommand(const std::string &name)
     {
         auto fn = shellCommands.find(name);
         if (fn != shellCommands.end()) {
@@ -15,7 +16,7 @@ namespace newsh {
         return std::make_pair(name, sh::stub);
     }
 
-    const char *getManUsage(const char *name)
+    std::string getManUsage(const std::string &name)
     {
         auto fn = manShellCommands.find(name);
         if (fn != manShellCommands.end()) {
@@ -25,7 +26,7 @@ namespace newsh {
         return "Not found\n";
     }
 
-    int dispatchCommand(std::pair<std::string, func> fn, char **args)
+    int dispatchCommand(std::pair<std::string, func> fn, std::vector<std::string> args)
     {
         func executeShellCommand = fn.second;
         
@@ -36,9 +37,9 @@ namespace newsh {
     /** SHELL FUNCTIONS **/
     namespace sh { 
     
-    int man(char **args)
+    int man(std::vector<std::string> args)
     {
-        if (!args[1]) {
+        if (args.size() < 2) {
             std::cerr << "expected argument to man\n";
             return 1;
         }
@@ -48,23 +49,24 @@ namespace newsh {
         return 1;
     }
    
-    int cd(char **args)
+    int cd(std::vector<std::string> args)
     {
-        if (!args[1]) {
+        if (args.size() < 2) {
             std::cerr << "expected argument to cd\n";
             return 1;
         }
         
-        if (chdir(args[1]) != 0) {
+        const char *dir = args[1].c_str();
+        if (chdir(dir) != 0) {
             perror("newsh cd");
         }
 
         return 1;
     }
 
-    int cat(char **args)
+    int cat(std::vector<std::string> args)
     {
-        if (!args[1]) {
+        if (args.size() < 2) {
             std::cerr << "expected argument to cat\n";
             return 1;
         }
@@ -79,7 +81,7 @@ namespace newsh {
         return 1;
     }
 
-    int stub(char **args)
+    int stub(std::vector<std::string> args)
     { 
 
 #if 1
